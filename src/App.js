fb5c8b4ace82228d0301c05667b26b85c1d95188
DESKTOP-3R1I2DP\Cristhian <deltapshi@gmail.com>
2025-05-03 12:59:46 -0600

    primer repositorio

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function App(){
+    
+    return(
+        <div>
+            <h1>Hola Mundo desde React</h1>
+            <P>Creando mi primera aplicacion desde react</P>
+        </div>
+ 
+    );
+}
