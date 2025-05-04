fd9273e3b0bec8b1286cea1b717f1e7712469585
DESKTOP-3R1I2DP\Cristhian <deltapshi@gmail.com>
2025-05-03 21:30:07 -0600

    Tarea 1 Hecha

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App(){
     return(
         <div>
             <h1>Hola Mundo desde React</h1>
-            <P>Creando mi primera aplicacion desde react</P>
+            <p>Creando mi primera aplicacion desde react</p>
         </div>
  
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-const root
-t = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
